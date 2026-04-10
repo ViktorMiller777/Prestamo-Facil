d3d98f00adbb9b97e61cac887100ec1e5976322c
Allixdx <allison.sanroman@outlook.com>
2026-04-10 21:58:16 +0000

    feat: integrar DigitalOcean Spaces para almacenamiento de archivos
    - Agregar dependency league/flysystem-aws-s3-v3
    - Configurar driver Spaces
    - Agregar endpoint CDN

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,18 @@ return [
             'report' => false,
         ],
 
+	'spaces' => [
+	    'driver' => 's3',
+	    'key' => env('DO_SPACES_KEY'),
+	    'secret' => env('DO_SPACES_SECRET'),
+	    'region' => env('DO_SPACES_REGION', 'sfo3'),
+	    'bucket' => env('DO_SPACES_BUCKET'),
+	    'endpoint' => env('DO_SPACES_ENDPOINT', 'https://sfo3.digitaloceanspaces.com'),
+	    'url' => env('DO_SPACES_CDN_URL'),
+	    'use_path_style_endpoint' => false,
+	],
+
+
     ],
 
     /*
